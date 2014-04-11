2eb732dccb6a6c0ed4ac20b06219d44b755cfb33
cvance <collin.vance@gmail.com>
2014-04-11 13:23:23 -0700

    import

--- source/org/jivesoftware/smack/proxy/DirectSocketFactory.java
+++ source/org/jivesoftware/smack/proxy/DirectSocketFactory.java
@@ -24,6 +24,7 @@ import java.net.Proxy;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import javax.net.SocketFactory;
+import org.jivesoftware.smack.SmackConfiguration;
 
 /**
  * SocketFactory for direct connection
