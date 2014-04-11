74e1a5928f497dcb1ee58680b1829537893c5fda
cvance <collin.vance@gmail.com>
2014-04-11 13:20:03 -0700

    removed custom constructors for socket factory

--- source/org/jivesoftware/smack/XMPPConnection.java
+++ source/org/jivesoftware/smack/XMPPConnection.java
@@ -563,7 +563,7 @@ public class XMPPConnection extends Connection {
                     this.socket = new Socket(host, port);
                 }
                 else {
-                    this.socket = config.getSocketFactory().createSocket(host, port, SmackConfiguration.getPacketReplyTimeout());
+                    this.socket = config.getSocketFactory().createSocket(host, port);
                 }
             } catch (UnknownHostException uhe) {
                 String errorMessage = "Could not connect to " + host + ":" + port + ".";
--- source/org/jivesoftware/smack/proxy/DirectSocketFactory.java
+++ source/org/jivesoftware/smack/proxy/DirectSocketFactory.java
@@ -40,12 +40,12 @@ class DirectSocketFactory
 
     static private int roundrobin = 0;
 
-    public Socket createSocket(String host, int port, int timeout)
+    public Socket createSocket(String host, int port)
         throws IOException, UnknownHostException
     {
         Socket newSocket = new Socket(Proxy.NO_PROXY);
         InetAddress resolved[] = InetAddress.getAllByName(host);
-        newSocket.connect(new InetSocketAddress(resolved[(roundrobin++) % resolved.length],port), timeout);
+        newSocket.connect(new InetSocketAddress(resolved[(roundrobin++) % resolved.length],port), SmackConfiguration.getPacketReplyTimeout());
         return newSocket;
     }
 
@@ -56,19 +56,11 @@ class DirectSocketFactory
         return new Socket(host,port,localHost,localPort);
     }
 
-    public Socket createSocket(InetAddress host, int port, int timeout)
-        throws IOException
-    {
-        Socket newSocket = new Socket(Proxy.NO_PROXY);
-        newSocket.connect(new InetSocketAddress(host,port), timeout);
-        return newSocket;
-    }
-
     public Socket createSocket(InetAddress host, int port)
             throws IOException
     {
         Socket newSocket = new Socket(Proxy.NO_PROXY);
-        newSocket.connect(new InetSocketAddress(host,port));
+        newSocket.connect(new InetSocketAddress(host,port), SmackConfiguration.getPacketReplyTimeout());
         return newSocket;
     }
 
